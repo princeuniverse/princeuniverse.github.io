3d68eb0fdf82e0d6c7a112316ffed86a29731d73
princeuniverse <princearya100@gmail.com>
2026-04-28 19:07:22 +0530

    Create deposit.js

---
+++ deposit.js
@@ -0,0 +1,8 @@
+function convertToWords() {
+    let num = document.getElementById("amount").value;
+    document.getElementById("words").value = num + " rupees only";
+}
+
+function saveData() {
+    alert("Deposit Saved");
+}
