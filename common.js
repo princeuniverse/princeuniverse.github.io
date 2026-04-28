bdf5c41f3b8e3741c8b9ccd337b3592e3566c32e
princeuniverse <princearya100@gmail.com>
2026-04-28 21:05:43 +0530

    Update common.js

--- common.js
+++ common.js
@@ -1,5 +1,25 @@
-function numberToWords(num) {
-    if (!num) return "";
+function convertToWords() {
+    let num = parseInt(document.getElementById("amount").value) || 0;
+    document.getElementById("words").value = numberToWords(num);
+}
 
-    return num + " rupees only"; // upgrade later
+function numberToWords(num) {
+    if (num === 0) return "Zero Rupees Only";
+
+    let a = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+        "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen",
+        "Seventeen", "Eighteen", "Nineteen"];
+
+    let b = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"];
+
+    function convert(n) {
+        if (n < 20) return a[n];
+        if (n < 100) return b[Math.floor(n / 10)] + " " + a[n % 10];
+        if (n < 1000) return a[Math.floor(n / 100)] + " Hundred " + convert(n % 100);
+        if (n < 100000) return convert(Math.floor(n / 1000)) + " Thousand " + convert(n % 1000);
+        if (n < 10000000) return convert(Math.floor(n / 100000)) + " Lakh " + convert(n % 100000);
+        return convert(Math.floor(n / 10000000)) + " Crore " + convert(n % 10000000);
+    }
+
+    return convert(num).trim() + " Rupees Only";
 }
