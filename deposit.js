53608bdece582203af14c234dd1e83e79d06a1d3
princeuniverse <princearya100@gmail.com>
2026-04-28 20:23:57 +0530

    Update deposit.js

--- deposit.js
+++ deposit.js
@@ -6,3 +6,38 @@ document.getElementById("amount").addEventListener("input", function () {
 function resetForm() {
     document.querySelectorAll(".form-card input").forEach(i => i.value = "");
 }
+
+
+function calcDeno() {
+    let rows = document.querySelectorAll(".deno-compact table tr");
+    let total = 0;
+
+    rows.forEach((row, i) => {
+        if (i === 0) return;
+
+        let inputs = row.querySelectorAll("input");
+        if (inputs.length < 2) return;
+
+        let val = parseInt(inputs[0].dataset.val);
+
+        let inVal = parseInt(inputs[0].value) || 0;
+        let outVal = parseInt(inputs[1].value) || 0;
+
+        let rowTotal = (inVal - outVal) * val;
+
+        row.querySelector(".t").innerText = rowTotal;
+
+        total += rowTotal;
+    });
+
+    document.getElementById("denoTotal").innerText = total;
+
+    // sync with amount
+    let amountBox = document.getElementById("amount");
+    if (amountBox) amountBox.value = total;
+
+    // update words
+    if (typeof convertToWords === "function") {
+        convertToWords();
+    }
+}
