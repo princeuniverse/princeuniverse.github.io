ed11b26fe0690b58af2a8831eeebdf40a282dc12
princeuniverse <princearya100@gmail.com>
2026-04-29 19:12:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -67,6 +67,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
+document.getElementById("toggleSidebar").addEventListener("click", function () {
+
+    document.querySelector(".sidebar").classList.toggle("collapsed");
+    document.querySelector(".content").classList.toggle("collapsed");
+
+});
+
+
+
     loadPage("home"); // default page
  });
 
