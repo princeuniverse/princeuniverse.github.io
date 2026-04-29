ef5f4418b3676eb00748ab16768929c1fc7e5104
princeuniverse <princearya100@gmail.com>
2026-04-29 18:24:22 +0530

    Create home.js

---
+++ js/home.js
@@ -0,0 +1,5 @@
+// demo data (later database se aayega)
+
+document.getElementById("totalTxn").innerText = 12;
+document.getElementById("totalDeposit").innerText = "₹ 25,000";
+document.getElementById("totalWithdraw").innerText = "₹ 8,000";
